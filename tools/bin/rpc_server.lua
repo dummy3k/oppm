e1d765f15a5a577ee3e7fc72c763ddd91b98a23d
dummy3k <l4711@gmx.net>
2018-07-22 22:02:22 +0200

    stuff

--- tools/bin/rpc_server.lua
+++ tools/bin/rpc_server.lua
@@ -1,13 +1,30 @@
-print("RPC Server v0.1")
+local serialization = require("serialization")
+local component = require("component")
+local event = require("event")
+
+local PORT = 123
+
+print("RPC Server v0.4")
+print("Adress", component.modem.address)
+
+if not component.modem.isOpen(PORT) then
+    print("opening port "..tostring(PORT))
+    if not component.modem.open(PORT) then error("open failed") end
+end
 
 while true do
     local _, _, from, port, _, message = event.pull("modem_message")
-    print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
+    -- print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
+    -- print(port, port)
+    -- print(message)
+    print(">"..from..":"..port)
+    print(">"..tostring(message))
+    
     local f = load(message)
-    -- f()
     local status, err = pcall(f)
-    print("status", status)
-    print("err", err)
+    print("<", status, err)
+    -- print("status", status)
+    -- print("err", err)
     if not status then
         local traceback = debug.traceback()
         -- print(debug.traceback())
--- tools/lib/tools.lua
+++ tools/lib/tools.lua
@@ -4,4 +4,83 @@ function tools.test()
     return 4712
 end
 
+function tools.compressSlots()
+    io.write("Compressing... ")
+  
+    local items = {}
+    local empty = {}
+    -- local items_count = {}
+    -- local items_maxSize = {}
+    -- local items_full = {}
+    for slot = 1, 16 do
+        local slot_info = component.inventory_controller.getStackInInternalSlot(slot)
+        if not slot_info then
+            table.insert(empty, slot)
+        else
+            local item = {maxCount = slot_info.maxSize, 
+                          full={},
+                          partial={},
+                          }
+            if not items[slot_info.label] then
+                items[slot_info.label] = item
+            else
+                item = items[slot_info.label]
+            end
+            
+            if (slot_info.maxSize - slot_info.size == 0) then
+                table.insert(item.full, slot)
+            else
+                table.insert(item.partial, {slot=slot, count=slot_info.size})
+            end
+        end
+    end
+    -- print(serialization.serialize(items))
+
+    for k, v in pairs(items) do
+        while #v.partial > 1 do
+            -- best first
+            table.sort(v, function(a, b) return a.count > b.count end )
+            local current = table.remove(v.partial, 1)
+            
+
+            
+            local missing = v.maxCount - current.count
+            -- print("missing", k, missing)
+            
+            table.sort(v, function(a, b) return a.count < b.count end ) -- small count first
+            local other = table.remove(v.partial, 1)
+            
+            
+            local transfer = math.min(missing, other.count)
+            -- print("transfer"..k, current.slot, other.slot, missing)
+            os.sleep(1)
+            if not robot.select(other.slot) then error("select failed") end
+            if not robot.transferTo(current.slot, transferr) then 
+                error("transferTo failed: "..tostring(current.slot)) 
+            end
+            
+            current.count = current.count + transfer
+            other.count = other.count - transfer
+            
+            if current.count < v.maxCount then
+                table.insert(v.partial, current)
+            else
+                table.insert(v.full, current.slot)
+            end
+            if other.count > 0 then
+                table.insert(v.partial, other)
+            end
+            
+        end
+    end
+    print("Done!")
+    for k, v in pairs(items) do
+        assert(#v.partial <= 1)
+        v.partial = v.partial[1]
+    end
+    return {items=items, empty=empty}
+end
+
+
+
 return tools
